4df02be3b355e4defbe0e17b675af62314e688e7
sobhan <imsobhanmoradi@gmail.com>
2026-05-31 21:08:55 +0430

    #10 commit

--- assets/js/my-procets.js
+++ assets/js/my-procets.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".collapsible");
   var instances = M.Collapsible.init(elems, {});
+
+  const addProjectBottun = document.getElementById("add_project_bottun");
+  addProjectBottun.addEventListener("click", () => {
+    document.querySelector(".add_project_modal").classList.add("show");
+    document
+      .querySelector(".add_project_modal_back")
+      .classList.remove("dis-none");
+  });
+
+  document
+    .querySelector(".add_project_modal_back")
+    .addEventListener("click", (e) => {
+      document.querySelector(".add_project_modal").classList.remove("show");
+      e.target.classList.add("dis-none");
+    });
 });
